f1b64aff52e8e50cc6f38e7ae31d24b138663896
Joseph Gammon <jgammon@local.uship.com>
2016-10-25 15:45:36 -0500

    moved extansion class to its own file

---
+++ src/uShip.Logging/LogBuilders/PropertiesDictionaryExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using log4net.Util;
+using uShip.Logging.Extensions;
+
+namespace uShip.Logging.LogBuilders
+{
+    public static class PropertiesDictionaryExtensions
+    {
+        private static string Sanitize(string input)
+        {
+            return input.IfNotNull(x => x.SanitizeSensitiveInfo().RemoveViewState());
+        }
+
+        public static void Set(this PropertiesDictionary dictionary, string key, object value)
+        {
+            if (value is string)
+            {
+                dictionary[key] = Sanitize(value.ToString());
+                return;
+            }
+            dictionary[key] = value;
+        }
+
+        internal static void SafeSetProp(this PropertiesDictionary props, string propKey, Func<string> valueGetter)
+        {
+            try
+            {
+                props.Set(propKey, valueGetter());
+            }
+            catch (Exception ex)
+            {
+                props.Set(propKey, string.Format("Failed setting {0} key in logger because {1}", propKey, ex.Message));
+            }
+        }
+    }
+}
